1e2b34516b146fdfc664aa72d95058897bb24ee9
Murat Oztemel <cegerxwin@gmail.com>
2024-05-15 05:49:50 +0300

    Sky and Bird animations start

--- src/models/Bird.jsx
+++ src/models/Bird.jsx
@@ -1,4 +1,21 @@
+import { useRef, useEffect } from "react";
+
+import birdScene from "../assets/3d/bird.glb";
+import { useAnimations, useGLTF } from "@react-three/drei";
+
 const Bird = () => {
-  return <div>Bird</div>;
+  const birdRef = useRef();
+  const { scene, animations } = useGLTF(birdScene);
+  const { actions } = useAnimations(animations, birdRef);
+
+  useEffect(() => {
+    actions["Take 001"].play();
+  }, []);
+
+  return (
+    <mesh position={[-5, 2, 1]} scale={[0.003, 0.003, 0.003]} ref={birdRef}>
+      <primitive object={scene} />
+    </mesh>
+  );
 };
 export default Bird;
